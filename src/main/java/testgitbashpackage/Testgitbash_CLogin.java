f6c1cb5827cfe419bf060ff7f399f15d5362932e
niveavenkat <nivea13v@gmail.com>
2022-05-22 11:54:07 +0530

    second commit

---
+++ src/main/java/testgitbashpackage/Testgitbash_CLogin.java
@@ -0,0 +1,42 @@
+package testgitbashpackage;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Testgitbash_CLogin {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// TODO Auto-generated method stub
+
+				//Launch Chromedriver
+				WebDriverManager.chromedriver().setup();
+				
+				//Handling notifications
+				ChromeOptions options = new ChromeOptions();
+			    options.addArguments("--disable-notifications");
+				ChromeDriver driver = new ChromeDriver(options);
+				
+				driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+				//Launch Salesforce app
+				driver.get("https://login.salesforce.com");
+				
+				//Maximize the screen
+				driver.manage().window().maximize();
+				
+				//Entering credentials and login
+			    driver.findElement(By.id("username")).sendKeys("mars@testleaf.com");
+			    driver.findElement(By.id("password")).sendKeys("BootcampSel$123");
+			    driver.findElement(By.id("Login")).click();
+			    
+		
+		
+	}
+
+}
